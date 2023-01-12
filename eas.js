05f147532d1e11f6a913ffb4f91fd6a41ed32869
Ahn <yunjessicaahn@gmail.com>
2023-01-12 13:13:17 -0800

    added findDivs function to grab divs that mouseover takes place

--- eas.js
+++ eas.js
@@ -23,3 +23,22 @@ for (let i=0; i <16*16; i++) {
         const divContainer = document.getElementById("container");
         divContainer.appendChild(iDiv);
     }
+
+    // 3. grab divs inside container and addEventListenr with mouseover effect
+document.addEventListener('mouseover', findDivs);
+// 3.1 find divs inside container
+// // 3.1.1 using pointerevent, find divs's target
+// // document.addEventListener ('click', (e)=>{console.log(e)});
+function findDivs (obj) {
+//     // 3.2 if divs's parent is container, 
+//     // 3.2.1 find divs that have container as parent
+    let divsParent = obj.target.parentNode;
+    // 3.2.2 console.log obj.target.innerHTML
+    let divsContext = obj.target.innerHTML;
+    let objTarget = obj.target;
+    if (obj.target == div){
+        console.log(divsContext);
+    // 3.3 change the backgroundColor of divsParent
+        obj.target.style.backgroundColor = "red";
+    }
+}
