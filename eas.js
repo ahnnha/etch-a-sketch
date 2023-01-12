9f99ac8ddd4e89899ba090b57603e66dccd0bcba
Ahn <yunjessicaahn@gmail.com>
2023-01-12 13:50:30 -0800

    added a new class name to event object

--- eas.js
+++ eas.js
@@ -13,8 +13,9 @@
 
 // 2. loop through process #1 sixteen times to create 16*16 grid
 for (let i=0; i <16*16; i++) {
-    // 2.1 create a div
+    // 2.1 create a div with className .myDivs
         let iDiv = document.createElement("div");
+        iDiv.className = "myDivs";
     // 2.2 add a word in the div
         let iDivWord = document.createTextNode(i);
     // 2.3 add the text to the div
@@ -25,20 +26,24 @@ for (let i=0; i <16*16; i++) {
     }
 
     // 3. grab divs inside container and addEventListenr with mouseover effect
-document.addEventListener('mouseover', findDivs);
-// 3.1 find divs inside container
-// // 3.1.1 using pointerevent, find divs's target
-// // document.addEventListener ('click', (e)=>{console.log(e)});
-function findDivs (obj) {
-//     // 3.2 if divs's parent is container, 
-//     // 3.2.1 find divs that have container as parent
-    let divsParent = obj.target.parentNode;
-    // 3.2.2 console.log obj.target.innerHTML
-    let divsContext = obj.target.innerHTML;
-    let objTarget = obj.target;
-    if (obj.target == div){
-        console.log(divsContext);
-    // 3.3 change the backgroundColor of divsParent
-        obj.target.style.backgroundColor = "red";
+    document.addEventListener('mouseover', findDivs);
+    // 3.1 declare divs that mouseover takes place
+    function findDivs (obj) {
+        // 3.1.1 ask computer what it thinks is obj
+        // console.log(obj);
+        // 3.2 print the number on each div if I declare divs correctly
+        let divsContext = obj.target.innerHTML;
+        // 3.1.2 if obj has the classname "myDivs", add a new class name
+        if (obj.target.classList.contains("myDivs")){
+            obj.target.classList.add("mouseoveredDivs");
+        // 3.1.3 check if obj has the new class name 
+            // if (obj.target.classList.contains("mouseoveredDivs")){
+            //     console.log("got ya");
+            // }
+        // 3.2 if adding additional class name doesn't work, we can change obj's background color in js
+            // obj.target.style.backgroundColor = "red";
+         } else {
+             console.log ("outside of grid");
+         }
     }
-}
+    
