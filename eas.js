315656fdd3c8deed1a163aaf82fa0f554c98f391
Ahn <yunjessicaahn@gmail.com>
2023-01-15 18:13:15 -0700

    connected css and js also added reset button

--- eas.js
+++ eas.js
@@ -10,18 +10,18 @@
 // divContainer.appendChild(firstDiv);
 
 // 2. loop through process #1 sixteen times to create 16*16 grid
-for (let i=0; i <16*16; i++) {
-    // 2.1 create a div with className .myDivs
-        let iDiv = document.createElement("div");
-        iDiv.className = "myDivs";
-    // 2.2 add a word in the div
-        let iDivWord = document.createTextNode(i);
-    // 2.3 add the text to the div
-        iDiv.appendChild(iDivWord);
-    // 2.4 put the Div inside "container" div
-        const divContainer = document.getElementById("container");
-        divContainer.appendChild(iDiv);
-    }
+// for (let i=0; i <cellNum*cellNum; i++) {
+//     // 2.1 create a div with className .myDivs
+//         let iDiv = document.createElement("div");
+//         iDiv.className = "myDivs";
+//     // 2.2 add a word in the div
+//         let iDivWord = document.createTextNode(i);
+//     // 2.3 add the text to the div
+//         iDiv.appendChild(iDivWord);
+//     // 2.4 put the Div inside "container" div
+//         const divContainer = document.getElementById("container");
+//         divContainer.appendChild(iDiv);
+//     }
 
     // 3. grab divs inside container and addEventListenr with mouseover effect
     document.addEventListener('mouseover', findDivs);
@@ -45,14 +45,66 @@ for (let i=0; i <16*16; i++) {
          }
     }
 
-    // 4.1 Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. 
-    // 4.1.1 declare button from html
-    const btn = document.getElementById("btn");
-    btn.addEventListener("click", askQuestion);
-    
-    function askQuestion () {
-        prompt ("Enter a number of strokes for each side");
+    // 5. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. 
+    // 5.2 connect css custom properties and js in global scope
+    let r = document.querySelector(":root");
+    let rs = getComputedStyle(r);
+    let rsColVal = rs.getPropertyValue("--numOfCol");
+    let rsRowVal = rs.getPropertyValue("--numOfRow");
+    // 5.3 make a function that will set new value for CSS property <--this doesn't work because numOfColumns is delcared inside function askQuestion()
+    function setCssValue () {
+        r.style.setProperty ("--numOfCol", numOfColumns);
+        console.log(rs.getPropertyValue("--numOfCol"));
+        r.style.setProperty ("--numOfRow", numOfRows);
+        console.log(rs.getPropertyValue("--numOfRow"));
     }
-    // 4.2 Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. 
+    // 5.4 make a function that will remove all the selected element to reset the screen
+    function removeGrid () {
+        // grab parent node 
+        let container = document.getElementById("container");
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+    // 4. Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. 
+    // 4.1 declare button from html
+    const btn = document.getElementById("btn");
+    // 4.2 add eventListener with function named askQuestion
+    btn.addEventListener("click", askQuestion);
+    // 4.3 create a function that asks brush size and store the answer
+    function askQuestion () {
+        // 5.1 reset numOfColumns and numOfRows to 0
+        let numOfColumns = 0;
+        let numOfRows = 0;
+        numOfColumns = prompt ("How many strokes to fill in the width?");
+        numOfRows = prompt("How many strokes to fill in the height?")
+        // 5.3 set numOfColumns and numOfRows to CSS root property
+        // setCssValue() <-- doesn't work because of scope
+        r.style.setProperty ("--numOfCol", numOfColumns);
+        // console.log(rs.getPropertyValue("--numOfCol"));
+        r.style.setProperty ("--numOfRow", numOfRows);
+        // console.log(rs.getPropertyValue("--numOfRow"));
+    // 4.4 using cellNum, create a grid
+        for (let i=0; i <numOfColumns*numOfRows; i++) {
+    // 2.1 create a div with className .myDivs
+        let iDiv = document.createElement("div");
+        iDiv.className = "myDivs";
+    // 2.2 add a word in the div
+        let iDivWord = document.createTextNode(i);
+    // 2.3 add the text to the div
+        iDiv.appendChild(iDivWord);
+    // 2.4 put the Div inside "container" div
+        const divContainer = document.getElementById("container");
+        divContainer.appendChild(iDiv);
+    }
+    }
+
+    // 6. Reset
+    let resetBtn = document.getElementById("resetBtn");
+    resetBtn.addEventListener("click", removeGrid);
+
+
+
+    
 
     
